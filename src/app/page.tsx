d6f7df998553a1416179c8a31595a46361230c5a
iankranz <ian@iankranz.com>
2025-12-17 16:55:54 -0600

    update colors and image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,15 @@ import styles from "./page.module.css"
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>Ian and Cindy&apos;s Website!</main>
+      <main className={styles.main}>
+        <h1>Happy holidays From Ian & Cindy</h1>
+        <Image
+          src="/cindo.png"
+          alt="Cindy dressed as a robber"
+          width={500}
+          height={726}
+        />
+      </main>
     </div>
   )
 }
